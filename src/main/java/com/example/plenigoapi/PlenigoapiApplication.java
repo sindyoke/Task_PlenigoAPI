32e252827226208728801db8f16a0135147e2c93
Sindy <sanjad@gmail.com>
2024-08-08 01:14:23 +0200

    first commit

---
+++ src/main/java/com/example/plenigoapi/PlenigoapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.plenigoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlenigoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlenigoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plenigoapi/PlenigoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plenigoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlenigoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
